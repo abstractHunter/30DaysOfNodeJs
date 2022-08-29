e98a30ea823b5363e28dfe376486e2b6419fab02
nextLine <daniel.attitsogbe02@gmail.com>
2022-08-29 23:29:39 +0000

    fixed some typos in index.js and index.html files

--- day_14/index.js
+++ day_14/index.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
